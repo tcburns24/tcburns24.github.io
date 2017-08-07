7d5a72eccfa4be2c9f31ab059eda2236b4865e5e
Thomas Burns <tcburns24@gmail.com>
2017-08-06 21:31:30 -0700

    new hover over

--- js/animation.js
+++ js/animation.js
@@ -1,11 +1,14 @@
 var proj = document.getElementsByClassName('project-thumb');
 
+var colors = ["#a9f7ff", "#89e8f7", "#6fe4ff", "#6bdcff", "#69caf1", "#6399d1"];
+
+
 for (var x=0; x<proj.length; x++) {
 	proj[x].addEventListener("mouseover", function() {
-		this.style.opacity="0.5";
+		this.style.borderColor = colors[Math.floor(Math.random() * colors.length)];
 	});
 	proj[x].addEventListener("mouseout", function() {
-		this.style.opacity = "1.0";
+		this.style.borderColor = "black";
 	});
 }
 
