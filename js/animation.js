f8108632d1c5bfe653e3cbd613d2988434efdc47
Thomas Burns <tcburns24@gmail.com>
2017-09-19 14:17:48 -0700

    project-thumb border thicker animation

--- js/animation.js
+++ js/animation.js
@@ -6,9 +6,11 @@ var colors = ["#a9f7ff", "#89e8f7", "#6fe4ff", "#6bdcff", "#69caf1", "#6399d1"];
 for (var x=0; x<proj.length; x++) {
 	proj[x].addEventListener("mouseover", function() {
 		this.style.borderColor = colors[Math.floor(Math.random() * colors.length)];
+		this.style.borderWidth = "12px";
 	});
 	proj[x].addEventListener("mouseout", function() {
 		this.style.borderColor = "black";
+		this.style.borderWidth = "thick";
 	});
 }
 
